a18f296294b46e33060b4c53977e0d06f72b71f9
Netta <c1nine00@students.oamk.fi>
2022-04-21 17:23:45 +0300

    New  html, css and js

---
+++ harjoitus2/script.js
@@ -0,0 +1,11 @@
+let h = document.getElementById("header");
+
+// h.classList.toggle("important"); //tilanteesta riippuen joko lisää tai poistaa
+//h.classList.add("important"); 
+//h.classList.add("strong");
+
+// h.classList.remove("important");
+
+h.style.color = "white";
+h.style.backgroundColor = "gray";
+h.style.fontSize = "40px";
