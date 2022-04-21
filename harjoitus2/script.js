de98da43e3e4e6785923ae79a1d62921d09861e5
Netta <c1nine00@students.oamk.fi>
2022-04-21 17:52:29 +0300

    added more in html and js

--- harjoitus2/script.js
+++ harjoitus2/script.js
@@ -1,4 +1,4 @@
-let h = document.getElementById("header");
+//let h = document.getElementById("header");
 
 // h.classList.toggle("important"); //tilanteesta riippuen joko lisää tai poistaa
 //h.classList.add("important"); 
@@ -6,6 +6,32 @@ let h = document.getElementById("header");
 
 // h.classList.remove("important");
 
-h.style.color = "white";
-h.style.backgroundColor = "gray";
-h.style.fontSize = "40px";
+//h.style.color = "white";
+//h.style.backgroundColor = "gray";
+//h.style.fontSize = "40px";
+
+
+
+//let button = document.getElementById("ok");
+
+
+//button.addEventListener("mouseover", doSome);
+//button.addEventListener("mouseleave", doSome);
+
+//button.addEventListener("mouseup", doSome);
+
+
+let div = document.getElementById("header");
+
+
+div.addEventListener("click", doSome);
+
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function doSome(event){
+    event.currentTarget.classList.add("strong");
+
+}
